32e72aab04c1c2b2876cb6c0c3935cce1718d50d
Pipeperez1305 <af.perezm1@uniandes.edu.co>
2025-05-25 22:25:03 -0500

    Implementation bq3_S4 Type 3

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,6 +9,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import data_loader
 from BQ_Sprint2 import bq1, bq2, bq3, bq4, bq5, bq6 
 from BQ_Sprint3 import bq6_S3, bq5_S3
+from BQ_Sprint4 import bq3_S4
 from firebase_config import db
 from google.cloud import firestore 
 
@@ -50,6 +51,13 @@ def pipeline():
     # Responder pregunta 6
     df_bq6_S3=bq6_S3.topSkills(data)
     exporter_csv.save_df_csv(df_bq6_S3, "bq6-S3Answer")
+    
+    # ---- Sprint 4 ----
+    # Responder pregunta 3
+    df_bq3_S4_summary, df_bq3_S4_registration_clicks, df_bq3_S4_eventDetail_clicks = bq3_S4.process_attendees_clicks(data)
+    exporter_csv.save_df_csv(df_bq3_S4_summary, "bq3-S4Summary")
+    exporter_csv.save_df_csv(df_bq3_S4_registration_clicks, "bq3_S4_registration_clicks")
+    exporter_csv.save_df_csv(df_bq3_S4_eventDetail_clicks, "bq3_S4_eventDetail_clicks")
     
     
 def pipeline_recommendation_bq2():
