4761854ffe3d3f5c182cd6a6ee8bae8de7280c16
dcamachom <88899391+dcamachom@users.noreply.github.com>
2025-05-26 20:32:42 -0500

    BQ4 done (#32)

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import data_loader
 from BQ_Sprint2 import bq1, bq2, bq3, bq4, bq5, bq6 
 from BQ_Sprint3 import bq6_S3, bq4_S3,bq5_S3, bq3_S3, bq7_S3, bq2_S3
-from BQ_Sprint4 import bq3_S4, bq2_S4
+from BQ_Sprint4 import bq3_S4, bq2_S4, bq4_S4
 from firebase_config import db
 from google.cloud import firestore 
 
@@ -75,6 +75,12 @@ def pipeline():
     exporter_csv.save_df_csv(df_bq3_S4_summary, "bq3-S4Summary")
     exporter_csv.save_df_csv(df_bq3_S4_registration_clicks, "bq3_S4_registration_clicks")
     exporter_csv.save_df_csv(df_bq3_S4_eventDetail_clicks, "bq3_S4_eventDetail_clicks")
+
+    #Responder pregunta 4
+    df_bq4_s4_summary,df_bq4_s4_views_per_day,df_bq_s4_views_per_day_by_type=bq4_S4.process_followers_following_logs(data)
+    exporter_csv.save_df_csv(df_bq4_s4_summary, "bq4-summary")
+    exporter_csv.save_df_csv(df_bq4_s4_views_per_day, "bq4-s4-views_per_day")
+    exporter_csv.save_df_csv(df_bq_s4_views_per_day_by_type, "bq4-views_per_day_by_type")
 
     # Sprint 4
 
